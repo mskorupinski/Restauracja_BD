ab4c1e6902e876c5757dddfdde59f904d93566f5
Marcin <m.skorupinski11@gmail.com>
2016-12-01 13:10:54 +0100

    panele

--- Restauracja/Restauracja/MainWindow.xaml.cs
+++ Restauracja/Restauracja/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace Restauracja
 {
@@ -25,9 +26,36 @@ namespace Restauracja
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void Zaloguj_Sie_Click(object sender, RoutedEventArgs e)
         {
+            dane logowanie = new dane();
+            SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-PMVJTLEG\\SQLSTANDARD;Network Library=dbmssocn;Initial Catalog = RESTAURACJA; User ID = " + Text_Logowanie.Text + ";Password = " + Text_Haslo.Password + ";");
+            try
+            {
 
+                // otwórz połączenie:
+                sqlConn.Open();
+                logowanie.Ustaw_login(Text_Logowanie.ToString());
+                logowanie.Ustaw_haslo(Text_Haslo.ToString());
+
+                sqlConn.Close();
+
+                Logowanie.Visibility = Visibility.Hidden;
+                Panel.Visibility = Visibility.Visible;
+            }
+            catch (System.Data.SqlClient.SqlException se)
+            {
+                MessageBox.Show(se.Message, se.Source);
+
+            }
+        }
+
+        class dane
+        {
+            string login = "";
+            string hasło = "";
+            public void Ustaw_login(string l) { login = l; }
+            public void Ustaw_haslo(string h) { hasło = h; }
         }
     }
 }
