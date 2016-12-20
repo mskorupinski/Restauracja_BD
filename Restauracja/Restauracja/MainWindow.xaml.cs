a6a2db4e5b58f4b5cad17de12f2a6bf2257034f9
Marcin <m.skorupinski11@gmail.com>
2016-12-20 13:12:13 +0100

    obrazki

--- Restauracja/Restauracja/MainWindow.xaml.cs
+++ Restauracja/Restauracja/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace Restauracja
     /// </summary>
     public partial class MainWindow : Window
     {
+        dane logowanie;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,7 +30,7 @@ namespace Restauracja
 
         private void Zaloguj_Sie_Click(object sender, RoutedEventArgs e)
         {
-            dane logowanie = new dane();
+             logowanie = new dane();
             SqlConnection sqlConn = new SqlConnection("Data Source=LAPTOP-PMVJTLEG\\SQLSTANDARD;Network Library=dbmssocn;Initial Catalog = RESTAURACJA; User ID = " + Text_Logowanie.Text + ";Password = " + Text_Haslo.Password + ";");
             try
             {
@@ -55,197 +56,27 @@ namespace Restauracja
         {
             string login = "";
             string hasło = "";
+
+            public string Login
+            {
+                get {
+                    return login;
+                }
+            }
+            public string Hasło
+            {
+                get
+                {
+                    return hasło;
+                }
+            }
             public void Ustaw_login(string l) { login = l; }
             public void Ustaw_haslo(string h) { hasło = h; }
         }
 
         private void Przycisk_Menu_Szukaj_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                SqlConnection sqlConn = new SqlConnection("Server = LocalHost;Integrated Security = SSPI; Database = 'Restauracja'");
-
-                sqlConn.Open();
-                SqlCommand sqlCmd = new SqlCommand();
-                sqlCmd.Connection = sqlConn;
-                SqlDataReader dataReader = null;
-                if (Tekst_Menu_Nr.Text != "")
-                {
-                    if (Tekst_Menu_Cena.Text != "")
-                    {
-
-                        if (Tekst_Menu_Opis.Text != "")
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where Id_menu=@Nr and Cena_produktu=@Cena and Opis_produktu=@Opis";
-
-                            SqlParameter param1 = new SqlParameter();
-                            param1.ParameterName = "@Nr";
-                            param1.Value = Tekst_Menu_Nr.Text;
-
-                            SqlParameter param2 = new SqlParameter();
-                            param2.ParameterName = "@Cena";
-                            param2.Value = Tekst_Menu_Cena.Text;
-
-                            SqlParameter param3 = new SqlParameter();
-                            param3.ParameterName = "@Opis";
-                            param3.Value = Tekst_Menu_Opis.Text;
-
-                            sqlCmd.Parameters.Add(param1);
-
-                            sqlCmd.Parameters.Add(param2);
-
-                            sqlCmd.Parameters.Add(param3);
-                            dataReader = sqlCmd.ExecuteReader();
-
-
-
-                        }
-                        else
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where Id_menu=@Nr and Cena_produktu=@Cena";
-
-                            SqlParameter param1 = new SqlParameter();
-                            param1.ParameterName = "@Nr";
-                            param1.Value = Tekst_Menu_Nr.Text;
-
-                            SqlParameter param2 = new SqlParameter();
-                            param2.ParameterName = "@Cena";
-                            param2.Value = Tekst_Menu_Cena.Text;
-
-                            sqlCmd.Parameters.Add(param1);
-
-                            sqlCmd.Parameters.Add(param2);
-
-                            dataReader = sqlCmd.ExecuteReader();
-                        }
-
-                    }
-                    else
-                    {
-                        if (Tekst_Menu_Opis.Text != "")
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where Id_menu=@Nr and  Opis_produktu=@Opis";
-
-                            SqlParameter param1 = new SqlParameter();
-                            param1.ParameterName = "@Nr";
-                            param1.Value = Tekst_Menu_Nr.Text;
-
-
-
-                            SqlParameter param3 = new SqlParameter();
-                            param3.ParameterName = "@Opis";
-                            param3.Value = Tekst_Menu_Opis.Text;
-
-                            sqlCmd.Parameters.Add(param1);
-
-
-                            sqlCmd.Parameters.Add(param3);
-                            dataReader = sqlCmd.ExecuteReader();
-
-                        }
-                        else
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where Id_menu=@Nr ";
-
-                            SqlParameter param1 = new SqlParameter();
-                            param1.ParameterName = "@Nr";
-                            param1.Value = Tekst_Menu_Nr.Text;
-
-
-
-                            sqlCmd.Parameters.Add(param1);
-
-
-                            dataReader = sqlCmd.ExecuteReader();
-
-
-                        }
-                    }
-                }
-                else
-                {
-
-                    if (Tekst_Menu_Cena.Text != "")
-                    {
-
-                        if (Tekst_Menu_Opis.Text != "")
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where  Cena_produktu=@Cena and Opis_produktu=@Opis";
-
-
-
-                            SqlParameter param2 = new SqlParameter();
-                            param2.ParameterName = "@Cena";
-                            param2.Value = Tekst_Menu_Cena.Text;
-
-                            SqlParameter param3 = new SqlParameter();
-                            param3.ParameterName = "@Opis";
-                            param3.Value = Tekst_Menu_Opis.Text;
-
-
-
-                            sqlCmd.Parameters.Add(param2);
-
-                            sqlCmd.Parameters.Add(param3);
-                            dataReader = sqlCmd.ExecuteReader();
-
-                        }
-                        else
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where  Cena_produktu=@Cena ";
-
-
-                            SqlParameter param2 = new SqlParameter();
-                            param2.ParameterName = "@Cena";
-                            param2.Value = Tekst_Menu_Cena.Text;
-
-
-
-                            sqlCmd.Parameters.Add(param2);
-
-                            dataReader = sqlCmd.ExecuteReader();
-                        }
-
-                    }
-                    else
-                    {
-                        if (Tekst_Menu_Opis.Text != "")
-                        {
-                            sqlCmd.CommandText = "select * from Produkt where  Opis_produktu=@Opis";
-
-
-                            SqlParameter param3 = new SqlParameter();
-                            param3.ParameterName = "@Opis";
-                            param3.Value = Tekst_Menu_Opis.Text;
-
-
-                            sqlCmd.Parameters.Add(param3);
-                            dataReader = sqlCmd.ExecuteReader();
-
-                        }
-                        else
-                        {
-                            MessageBox.Show("Musisz podać parametry!", "Błąd");
-                        }
-                    }
-
-
-                }
-                if (dataReader != null)
-                {
-                    DataTable dt = new DataTable();
-                    dt.Load(dataReader);
-                    Dane_Menu.ItemsSource = dt.DefaultView;
-
-                    dataReader.Close();
-                }
-                sqlConn.Close();
-            }
-            catch (System.Data.SqlClient.SqlException ex)
-            {
-
-                MessageBox.Show(ex.Message, "Błąd");
-            }
+            
         }
 
         private void Tekst_Menu_Nr_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -267,110 +98,58 @@ namespace Restauracja
 
         private void Przycisk_Menu_Dodaj_Click(object sender, RoutedEventArgs e)
         {
-            if (Tekst_Menu_Nr.Text != "")
+           
+        }
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+        }
+
+        private void comboBox_DropDownOpened(object sender, EventArgs e)
+        {
+            if (comboBox.Items.Count > 0) comboBox.Items.Clear();
+                
+            string Sql = "select Opis from Menu";
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-PMVJTLEG\\SQLSTANDARD;Network Library=dbmssocn;Initial Catalog = RESTAURACJA; User ID = marcin ;Password = marcin;");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(Sql, conn);
+            SqlDataReader DR = cmd.ExecuteReader();
+
+            while (DR.Read())
             {
-                if (Tekst_Menu_Cena.Text != "")
-                {
-
-                    if (Tekst_Menu_Opis.Text == "")
-                    {
-
-                        MessageBox.Show("Musisz podać opis!", "Błąd");
-
-
-                    }
-                    else
-                    {
-                        /*
-                        SqlConnection sqlConn=null;
-                        try
-                        {
-                             sqlConn = new SqlConnection("Server = LocalHost; Integrated Security = SSPI; Database = 'Restauracja'");
-
-                            // otwórz połączenie:
-                            sqlConn.Open();
-                            SqlCommand sqlCmd = new SqlCommand();
-                            sqlCmd.CommandText = "INSERT into Produkt (Id_menu, Cena_produktu , Opis_produktu) " +
-                   " VALUES ('" + Tekst_Menu_Nr.Text + "', '" + Tekst_Menu_Cena.Text + "', '" + Tekst_Menu_Opis.Text + "');";
-                            sqlCmd.Connection = sqlConn;
-                            sqlCmd.ExecuteNonQuery();
-
-
-                            // zamknij połaczenie:
-                            sqlConn.Close();
-                        }
-                        catch
-                        {
-                            
-
-                            MessageBox.Show("Error to save on database");
-                            if (sqlConn != null)
-                            {
-                                sqlConn.Close();
-                            }
-                            
-                        }*/
-
-                    }
-
-
-                }
-                else
-                {
-                    if (Tekst_Menu_Opis.Text != "")
-                    {
-                        MessageBox.Show("Musisz podać cenę!", "Błąd");
-
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("Musisz podać cenę i opis!", "Błąd");
-
-
-
-                    }
-                }
-            }
-            else
-            {
-
-                if (Tekst_Menu_Cena.Text != "")
-                {
-
-                    if (Tekst_Menu_Opis.Text != "")
-                    {
-
-
-                        MessageBox.Show("Musisz podać numer menu!", "Błąd");
-
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("Musisz podać numer menu i opis!", "Błąd");
-
-                    }
-
-                }
-                else
-                {
-                    if (Tekst_Menu_Opis.Text != "")
-                    {
-                        MessageBox.Show("Musisz podać numer menu i cenę!", "Błąd");
-
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("Musisz podać parametry!", "Błąd");
-
-                    }
-                }
-
+                comboBox.Items.Add(DR[0]);
 
             }
 
+            conn.Close();
+        }
+
+        private void comboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            SqlConnection sqlConn = new SqlConnection("Server = LocalHost;Integrated Security = SSPI; Database = 'Restauracja'");
+
+            sqlConn.Open();
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.Connection = sqlConn;
+            SqlDataReader dataReader = null;
+
+            Tekst_Menu_Cena.Text = comboBox.Text;
+
+            sqlCmd.CommandText = "select Opis_produktu as Opis , Cena_produktu as Cena  from Produkt as p join  Menu as m on p.Id_menu=m.Id_menu where m.Opis='" + comboBox.Text + "'";
+
+            dataReader = sqlCmd.ExecuteReader();
+
+            if (dataReader != null)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(dataReader);
+                Dane_Menu.ItemsSource = dt.DefaultView;
+
+                dataReader.Close();
+            }
+            sqlConn.Close();
         }
     }
 }
